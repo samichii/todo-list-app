02e351c5715deef6df7646f730d3defbc732b8f0
sami <sophiamanagbanag@gmail.com>
2025-05-28 08:26:54 +0800

    My initial server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+ 
+const app = express();
+ 
+// define the port
+const port = 3000;
+ 
+app.get("/", (req, res) => {
+  res.send("Hello Todo App!!!");
+});
+ 
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
